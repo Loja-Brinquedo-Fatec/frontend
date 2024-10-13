7c855ae3bec087606b7cbf3f0661591be64a29d9
Smil <149633447+IsaiasSorriso@users.noreply.github.com>
2024-10-13 12:59:39 -0300

    fé

--- loja-brinquedo/src/App.vue
+++ loja-brinquedo/src/App.vue
@@ -1,27 +1,25 @@
 <template>
   <div id="app">
-    <!-- Componente de Cabeçalho -->
+
     <Header />
 
-    <!-- Renderiza a página correspondente à rota atual -->
     <router-view />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import Header from './components/Header.vue'; // Certifique-se de que o caminho está correto
+import Header from './components/Header.vue'; 
 
 export default defineComponent({
   name: 'App',
   components: {
-    Header, // Registra o componente Header
+    Header, 
   },
 });
 </script>
 
 <style>
-/* Estilos Globais */
 body {
   margin: 0;
   font-family: Arial, sans-serif;
